52c7ef64220f227c1394fc168bdfe572464ed621
root <root@ip-172-31-28-253.us-west-2.compute.internal>
2013-10-19 04:14:42 +0000

    CMG-101 Combining html templates. Adding initial styles for story page. Styles for Breadcrumb.

--- src/js/uidevtest.js
+++ src/js/uidevtest.js
@@ -1,23 +1,12 @@
 $(function() {
-	
-	//Load list on page load
-	$.getJSON( "../js/uidevtest-data.js", function( data ) {		
-	    $.Mustache.load('list.html')
-    	.done(function () {
-			//Set variable for last item in list
-			for(var i=0, j=data.objects.length;i<j; i++){
-				data.objects[i].pub_date = _cmg.formatDate( data.objects[i].pub_date );
-				data.objects[i].updated = _cmg.formatDate( data.objects[i].updated );
-			}
-        	$('#page').empty().mustache('list', data);
-        });
-	});   
-	
-
-	
+		
 	//set up namespace
     var _cmg = {};
+	_cmg.properties = {};
+	_cmg.stories = {};
+	_cmg.stories.story = [];
 	
+	//Format Date		
 	_cmg.formatDate = function(date) {
 	    var d = new Date(date);
 		console.log(date);
@@ -68,6 +57,85 @@ $(function() {
 		
 	    return h+":"+m+" "+dd+" "+day+", "+ month +". "+d.getDay()+", "+d.getFullYear();
 	}
+	
+	// Get params from the url
+	_cmg.getUrlVars = function()
+	{
+	    var vars = [], hash;
+	    var hashes = window.location.href.slice(window.location.href
+	                     .indexOf('?') + 1).split('&');
+
+	    for(var i = 0; i < hashes.length; i++)
+	    {
+	        hash = hashes[i].split('=');
+	        vars.push(hash[0]);
+	        vars[hash[0]] = hash[1];
+	    }
+
+	    return vars;
+	}
+	
+	// handle param location
+	_cmg.handleLocation= function(data)
+	{
+		
+		var storyLocation = '';
+		//Loop over stories and check if the story id matches whats in query param
+		for(var i=0, j=_cmg.stories.story.length;i<j; i++){
+			if(_cmg.stories.story[i].id == data['story']){
+				storyLocation = i;
+			};
+		} 
+		//If Story Parameter exsits
+		if( storyLocation || storyLocation === 0 ){		
+			console.log(_cmg.stories.story[storyLocation]);
+			_cmg.showStoryView( _cmg.stories.story[storyLocation] );
+		//No param load default list view	
+		}else{
+			_cmg.showListView();
+		}
+	}
+		
+	//Check url for params
+	_cmg.properties.params = _cmg.getUrlVars();
+	
+
+	
+	//Load List View
+	_cmg.showListView = function(){	
+		console.log(_cmg);	
+			//Render the summaries on the page.
+        	$('#page').empty().mustache('list', _cmg.stories);		
+	}
+	
+	//Load Story View
+	_cmg.showStoryView = function(story){		
+			//Render the summaries on the page.
+        	$('#page').empty().mustache('story', story);
+	}
+	
+	
+	///// Kick things off ////
+	//Get and Set Story Data
+	$.getJSON( "../js/uidevtest-data.js", function( data ) {		
+		//Set variable for last item in list
+		for(var i=0, j=data.objects.length;i<j; i++){
+			data.objects[i].pub_date = _cmg.formatDate( data.objects[i].pub_date );
+			data.objects[i].updated = _cmg.formatDate( data.objects[i].updated );
+			var k = i+1;
+			var storyNum = k < 10 ? "0" + k : k;
+			_cmg.stories.story[i] = data.objects[i];
+			_cmg.stories.story[i].id = 'sto' +  storyNum;			
+		}
+		
+		//Load Templates
+		$.Mustache.addFromDom();
+		
+		//Handle location
+		_cmg.handleLocation( _cmg.properties.params ); 				
+	}); 
+	
+ 
 	
 
 
